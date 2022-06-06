ee03930f9f280f9fd591efe67fe65fcdae3d41aa
alessiamattesini <60142505+alessiamattesini@users.noreply.github.com>
2022-06-06 15:49:42 +0200

    yay

--- new.js
+++ new.js
@@ -5,8 +5,10 @@ var go_on = 0;
 var text_box = "";
 var start = false;
 
-var larghezza;
-
+var iniziamo = false;
+var press_enter = false;
+var fade = 0;
+var fadeAmount = 1;
 
 
 function preload() {
@@ -28,24 +30,56 @@ function setup() {
 
   //write your name
   yourname = createInput();
-  yourname.style("color:rgba(212, 255, 164, 1); background:rgba(212, 255, 164, 0); border-radius: 50px; border: 1px solid #D4FFA4; width: 244px; height: 43px; padding-left: 10px; font-size: 20px;text-align: center; font-family:Nunito;");
+  yourname.style("color:rgba(212, 255, 164, 1); background:rgba(212, 255, 164, 0); border-radius: 50px; border: 1px solid #D4FFA4; width: 244px; height: 43px; padding-left: 10px; font-size: 17px;text-align: center; font-family:Nunito;");
   yourname.position((windowWidth / 2) - 244 / 2, windowHeight/2 + 10);
   yourname.center('horizontal');
 
 
   //submit
   button = createButton('lets go');
-  larghezza = button.width;
-  button.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 244px; height: 43px; padding-left: 10px; font-size: 20px; font-family:Nunito;");
+  button.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 244px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
   button.position(windowWidth / 2, windowHeight*4/5);
   button.center('horizontal');
   button.mousePressed(name);
 
-  //pronouns
-  
+  // //pronouns
+  // she = createButton('she/her');
+  // she.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+  // she.position(windowWidth/5 - 80, windowHeight * 2 / 3);
+  // she.mouseOver(im_over);
+  // she.mouseOut(im_out);
+  // //she.mousePressed(she_select);
+  //
+  // he = createButton('he/him');
+  // he.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+  // he.class('he_pron');
+  // he.position(windowWidth *  2/5 - 80, windowHeight * 2 / 3);
+  // he.mouseOver(im_over1);
+  // he.mouseOut(im_out1);
+  // he.mousePressed(he_select);
+  //
+  // they = createButton('they/them');
+  // they.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+  // they.class('he_pron');
+  // they.position(windowWidth *  3/5 - 80, windowHeight * 2 / 3);
+  // they.mouseOver(im_over2);
+  // they.mouseOut(im_out2);
+  //
+  // ze = createButton('ze/zir');
+  // ze.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+  // ze.class('he_pron');
+  // ze.position(windowWidth *  4/5 - 80, windowHeight * 2 / 3);
+  // ze.mouseOver(im_over3);
+  // ze.mouseOut(im_out3);
 
 
+/////CHAPTER 1
 
+  chapter_1 = createVideo('./assets_new/video/segnaposto.mp4');
+  chapter_1.hide();
+
+  rin_vision = createVideo('./assets_new/video/rin-vision.mp4');
+  rin_vision.hide();
 
 }
 
@@ -66,18 +100,89 @@ function draw() {
 
   }
 
+  if(start == true && go_on == 0){
+
+    image(chapter_1, 0, 0, windowWidth, windowHeight);
+
+  }
+
+  if(iniziamo == true && go_on == 0){
+
+    image(rin_vision, 0, 0, windowWidth, windowHeight);
+
+  }
+
+  if(press_enter == true && go_on == 0){
+
+    background(0);
+
+    if (fade < 10 ){
+      fadeAmount = 3;
+    }
+    if (fade > 255){
+      fadeAmount = -3;
+    }
+    fade += fadeAmount;
+
+   push();
+   enter = "Press ENTER to go on";
+   textFont('Nunito');
+   textAlign(CENTER);
+   fill(212, 255, 164, fade);
+   textSize(30);
+   text(enter, windowWidth/2, windowHeight/2);
+   pop();
+
+
+
+  }
+
 }
 
 function name() {
 
   player_name = yourname.value();
   yourname.hide();
-  //write.hide();
   button.hide();
   console.log(player_name);
 
+  start = true;
+
+  chapter_1.play();
+  chapter_1.onended(video_end);
+
 }
 
+function video_end(){
+
+  iniziamo = true;
+  start = false;
+
+  rin_vision.play();
+  rin_vision.onended(vision_end);
+
+}
+
+function vision_end(){
+
+  iniziamo = false;
+  press_enter = true;
+
+}
+
+
+
+function keyPressed(){
+
+  if(keyCode == 13){
+
+    if(press_enter == true){
+
+      go_on++;
+
+    }
+  }
+}
 
 //window resize
 function windowResized() {
@@ -86,3 +191,62 @@ function windowResized() {
   background('black');
 
 }
+
+// function im_over(){
+//
+//   she.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_out(){
+//
+//   she.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_over1(){
+//
+//   he.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_out1(){
+//
+//   he.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_over2(){
+//
+//   they.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_out2(){
+//
+//   they.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_over3(){
+//
+//   ze.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0.3); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function im_out3(){
+//
+//   ze.style("border-radius: 50px; border: 1px solid #D4FFA4; background:rgba(212, 255, 164, 0); color: #D4FFA4; width: 160px; height: 43px; padding-left: 10px; font-size: 17px; font-family:Nunito;");
+//
+// }
+//
+// function he_select(){
+//
+//   he = true;
+//   she = false;
+//   ze = false;
+//   they = false;
+//
+//   he.style
+//
+// }
