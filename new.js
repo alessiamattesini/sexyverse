f7ad585c93827f559b10e9b96393183256c4b8e2
alessiamattesini <60142505+alessiamattesini@users.noreply.github.com>
2022-06-12 17:16:15 +0200

    fino al desktop

--- new.js
+++ new.js
@@ -10,6 +10,11 @@ var press_enter = false;
 var fade = 0;
 var fadeAmount = 1;
 
+//DESKTOP variabili
+
+var totem_pdf = false;
+var next_rev = false;
+
 
 function preload() {
 
@@ -24,6 +29,7 @@ function setup() {
   welcome_page = loadImage("./assets_new/img/WELCOME.png");
   rin_lab = loadImage("./assets_new/img/rin_lab.png");
   archive_door = loadImage("./assets_new/img/archive_door.png");
+  archive = loadImage("./assets_new/img/archive.png");
   star_S = loadImage("./assets_new/img/star_S.png");
   star_M = loadImage("./assets_new/img/star_M.png");
 
@@ -92,6 +98,18 @@ function setup() {
   rin_vision = createVideo('./assets_new/video/rin-vision.mp4');
   rin_vision.hide();
 
+///////////checkbox
+
+  checkbox = createCheckbox('I accept the sexy rulez <3', false);
+  checkbox.position((width/2) - checkbox.width/9, height * 8 / 12 - 60);
+  //checkbox.changed(myCheckedEvent);
+  checkbox.style('color: rgba(53, 28, 117, 1); font-size: 25px ; font-family: Nunito; text-align : right');
+  checkbox.class('checky');
+  // checkbox.style('font-size', '25px');
+  // checkbox.style('font-family', 'Nunito');
+  // checkbox.style('text-align', 'right');
+  checkbox.hide();
+
 
 /////AFTER RIN Vision
 
@@ -99,6 +117,23 @@ function setup() {
   rin_worried = loadImage("./assets_new/char/rin2.png");
   rin_talk = loadImage("./assets_new/char/rin3.png");
 
+////////////////////ARCHIVE DESKTOP
+  folder = createImg("./assets_new/img/folder.png");
+  folder.position(windowWidth/2, windowHeight/2);
+  folder.doubleClicked(apri_cartella);
+  folder.size(179 * 0.6, 156 * 0.6);
+  folder.hide();
+
+  condom_bin = createImg("./assets_new/img/condom_bin.png");
+  condom_bin.position(windowWidth * 4/5 - 45, windowHeight * 9/13);
+  condom_bin.size(158 * 0.6, 179 * 0.6);
+  condom_bin.hide();
+
+  hazard = createImg("./assets_new/img/hazard.png");
+  hazard.position(windowWidth * 2/9, windowHeight/4);
+  hazard.size(165 * 0.6, 163 * 0.6);
+  hazard.doubleClicked(revolution);
+  hazard.hide();
 
 
 }
@@ -717,6 +752,320 @@ function draw() {
 
   }
 
+  if(go_on == 11){
+
+    image(archive_door, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+    //TEXT BOX
+    push();
+    fill('rgba(69, 66, 168, 1)');
+    stroke('rgba(252, 252, 167, 1)');
+    strokeWeight(1.5);
+    rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    pop();
+
+    image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+
+    //NAME CHARACTER
+    push();
+    rin_name = "RIN";
+    textFont('Nunito');
+    textStyle(BOLD);
+    textAlign(LEFT);
+    textSize(20);
+    fill('rgba(252, 252, 167, 1)');
+    text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    pop();
+
+    //TEXT
+    push();
+    text_box = "Please, fill out this form on the door and read the archive rules. ";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+
+  }
+
+  if(go_on == 11){
+
+    // image(archive_door, 0, 0, windowWidth, windowHeight);
+    // image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+    //
+    // //TEXT BOX
+    // push();
+    // fill('rgba(69, 66, 168, 1)');
+    // stroke('rgba(252, 252, 167, 1)');
+    // strokeWeight(1.5);
+    // rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    // pop();
+    //
+    // image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    // image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    // image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+    //
+    // //NAME CHARACTER
+    // push();
+    // rin_name = "RIN";
+    // textFont('Nunito');
+    // textStyle(BOLD);
+    // textAlign(LEFT);
+    // textSize(20);
+    // fill('rgba(252, 252, 167, 1)');
+    // text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    // pop();
+
+    background(0);
+
+    //TEXT
+    push();
+    text_box = "Questo è un segna posto in attesa di capire come saranno le rulez";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+
+  }
+
+  if(go_on == 12){
+
+    image(archive_door, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+    //TEXT BOX
+    push();
+    fill('rgba(69, 66, 168, 1)');
+    stroke('rgba(252, 252, 167, 1)');
+    strokeWeight(1.5);
+    rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    pop();
+
+    image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+
+    //NAME CHARACTER
+    push();
+    rin_name = "RIN";
+    textFont('Nunito');
+    textStyle(BOLD);
+    textAlign(LEFT);
+    textSize(20);
+    fill('rgba(252, 252, 167, 1)');
+    text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    pop();
+
+    //TEXT
+    push();
+    text_box = "Great! I'm glad you accepted, you're so hot! Welcome in privacy sex archive!";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+
+  }
+
+
+//////////////ENTERING THE  ARCHIVE
+  if(go_on == 13){
+
+    image(archive, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+    //TEXT BOX
+    push();
+    fill('rgba(69, 66, 168, 1)');
+    stroke('rgba(252, 252, 167, 1)');
+    strokeWeight(1.5);
+    rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    pop();
+
+    image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+
+    //NAME CHARACTER
+    push();
+    rin_name = "RIN";
+    textFont('Nunito');
+    textStyle(BOLD);
+    textAlign(LEFT);
+    textSize(20);
+    fill('rgba(252, 252, 167, 1)');
+    text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    pop();
+
+    //TEXT
+    push();
+    text_box = "The archive is full of books and objects that tell sexyverse stories.";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+
+  }
+
+  if(go_on == 14){
+
+    image(archive, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+    //TEXT BOX
+    push();
+    fill('rgba(69, 66, 168, 1)');
+    stroke('rgba(252, 252, 167, 1)');
+    strokeWeight(1.5);
+    rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    pop();
+
+    image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+
+    //NAME CHARACTER
+    push();
+    rin_name = "RIN";
+    textFont('Nunito');
+    textStyle(BOLD);
+    textAlign(LEFT);
+    textSize(20);
+    fill('rgba(252, 252, 167, 1)');
+    text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    pop();
+
+    //TEXT
+    push();
+    text_box = "Most of them have been digitalized and are now accessible on my Iplug pro computer.";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+
+  }
+
+  if(go_on == 14){
+
+    image(archive, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+    //TEXT BOX
+    push();
+    fill('rgba(69, 66, 168, 1)');
+    stroke('rgba(252, 252, 167, 1)');
+    strokeWeight(1.5);
+    rect(windowWidth/10, windowHeight - 200, 650, 239/2, 235/2, 235/2);
+    pop();
+
+    image(star_M, windowWidth/10 -  5, windowHeight - 215, star_M.width/2, star_M.height/2);
+    image(star_S, windowWidth/10 + 615 - star_S.width/4, windowHeight - 235 + star_S.height/4 + 239/2 , star_S.width/2, star_S.height/2);
+    image(star_S, windowWidth/10 + star_S.width/2, windowHeight - 200 -  star_S.height/4, star_S.width/2, star_S.height/2);
+
+    //NAME CHARACTER
+    push();
+    rin_name = "RIN";
+    textFont('Nunito');
+    textStyle(BOLD);
+    textAlign(LEFT);
+    textSize(20);
+    fill('rgba(252, 252, 167, 1)');
+    text(rin_name, windowWidth/10 + 50, windowHeight - 203 + 20, 600, 239/2 - 50);
+    pop();
+
+    //TEXT
+    push();
+    text_box = "Give it a grope... ehm... I mean search through the files to find information to decode my vision.";
+    textFont('Nunito');
+    textAlign(LEFT);
+    fill(255);
+    textSize(20);
+    text(text_box, windowWidth/10 + 50, windowHeight - 173 + 20, 550, 239/2 - 50);
+    pop();
+
+    if (fade < 10 ){
+      fadeAmount = 3;
+    }
+    if (fade > 255){
+      fadeAmount = -3;
+    }
+    fade += fadeAmount;
+
+   push();
+   enter = "Press ENTER to get to the Iplug Pro";
+   textFont('Nunito');
+   textAlign(LEFT);
+   fill(252, 252, 167, fade);
+   textSize(20);
+   text(enter, windowWidth/10 + 50, windowHeight - 223);
+   pop();
+
+
+  }
+
+  if(go_on == 15){
+
+    image(archive, 0, 0, windowWidth, windowHeight);
+    image(rin, windowWidth * 3/5 , windowHeight - rin.width, rin.width/2, rin.height/2 );
+
+////////////////black layer
+    push();
+    fill('rgba(0, 0, 0, 0.5)');
+    rect(0,  0, windowWidth, windowHeight);
+    pop();
+
+
+/////////////DESKTOP
+    condom_bin.show();
+    folder.show();
+    hazard.show();
+
+    push();
+    label_1 =  "condom bin";
+    textFont('Nunito');
+    textAlign(CENTER);
+    textSize(16);
+    fill('white');
+    text(label_1, windowWidth * 4/5 - 45, windowHeight * 9/13 + 100, 158 * 0.6, 179*0.6);
+    pop();
+
+    push();
+    label_2 =  "AWKW_chaces";
+    textFont('Nunito');
+    textAlign(CENTER);
+    textSize(16);
+    fill('white');
+    text(label_2, windowWidth * 2/9, windowHeight/4 + 80, 165 * 0.6, 163 * 0.6);
+    pop();
+
+    push();
+    label_3 =  "TOTEM.pdf";
+    textFont('Nunito');
+    textAlign(CENTER);
+    textSize(16);
+    fill('white');
+    text(label_3, windowWidth/2, windowHeight/2 + 80, 179 * 0.6, 156 * 0.6);
+    pop();
+
+  }
+
+
 
 
 }
@@ -764,6 +1113,19 @@ function keyPressed(){
 
     }
   }
+}
+
+function apri_cartella(){
+
+  console.log("l'ho aperta");
+  totem_pdf =  true;
+
+}
+
+function revolution(){
+
+  rev_1 = true;
+
 }
 
 //window resize
