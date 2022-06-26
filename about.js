3c179e6e3dcc9b8b8015d33965e4c5a7db060091
annagargantini <90716011+annagargantini@users.noreply.github.com>
2022-06-26 22:20:25 +0200

    aggiunto about

--- about.js
+++ about.js
@@ -1,31 +1,31 @@
 function setup() {
 
-  createCanvas(windowWidth,windowHeight);
-
-
-  welcome = createImg("./assets_new/img/WELCOME.png");
-
-
-  text = createP("Siamo molto karyne");
-  text.position(windowWidth/2 - 100, windowHeight/3);
-  text.style("color:white; font-size:30px; font-weight: bold;");
-  text.center();
+  // createCanvas(windowWidth,windowHeight);
+  //
+  //
+  // welcome = createImg("./assets_new/img/WELCOME.png");
+  //
+  //
+  // text = createP("Siamo molto karyne");
+  // text.position(windowWidth/2 - 100, windowHeight/3);
+  // text.style("color:white; font-size:30px; font-weight: bold;");
+  // text.center();
 
 
 }
 
 function draw() {
 
-  image(welcome, 0, 0, windowWidth, windowHeight);
+  // image(welcome, 0, 0, windowWidth, windowHeight);
 
 
 }
 
 
-
-function windowResized(){
-
-      resizeCanvas(windowWidth, windowHeight);
-      background('black');
-
-    }
+// 
+// function windowResized(){
+//
+//       resizeCanvas(windowWidth, windowHeight);
+//       background('black');
+//
+//     }
